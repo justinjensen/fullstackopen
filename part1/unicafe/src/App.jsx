ef0e197bb53a0da8f700979cd2248cc059b4cd96
justinjensen <justinjensen@users.noreply.github.com>
2024-11-22 03:11:04 -0700

    exercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,9 +5,12 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / all || 0;
   const positive = (good / all) * 100 || 0;
 
+  if (all === 0) {
+    return <div>No feedback given</div>;
+  }
+
   return (
     <>
-      <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
@@ -30,6 +33,7 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
