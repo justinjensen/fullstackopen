8ff8dcbecf436ffbf8a28240a563683f96f53751
justinjensen <justinjensen@users.noreply.github.com>
2024-11-22 16:25:40 -0700

    exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,43 +1,4 @@
-const Header = ({ name }) => {
-  return <h1>{name}</h1>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </>
-  );
-};
-
-const Total = ({ parts }) => {
-  let total = parts.reduce((t, p) => t + p.exercises, 0);
-  return (
-    <p>
-      <strong>total of {total} exercises</strong>
-    </p>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
